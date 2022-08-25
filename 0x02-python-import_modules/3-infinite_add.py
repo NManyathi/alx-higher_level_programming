5fec1f3e0760d9c3dfb2b762761f4ef7c453634f
NManyathi <wn.manyathi@gmail.com>
2022-08-25 14:56:00 -0700

    Task 3. Infinite addition

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,14 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys
-    nargs = len(sys.argv) - 1
-    if (nargs == 0):
-        print("{:d} arguments.".format(nargs))
-    elif (nargs == 1):
-        print("{:d} argument:".format(nargs))
-    else:
-        print("{:d} arguments:".format(nargs))
-    for str in range(len(sys.argv)):
-        if (str == 0):
-            continue
-        print("{:d}: {:s}".format(str, sys.argv[str]))
+    from sys import argv
+    add = 0
+    for s in argv[1:]:
+        add += int(s)
+    print("{:d}".format(add))
